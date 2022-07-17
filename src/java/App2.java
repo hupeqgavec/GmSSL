0fd106bffa2ccb473b804a0f55829c146dd52699
hupeqgavec <113158210+hupeqgavec@users.noreply.github.com>
2022-07-17 13:05:24 +0000

    fix bug in function (2022-07-17T13:05:24)

--- src/java/App2.java
+++ src/java/App2.java
@@ -4,3 +4,4 @@ public class App56 { public static void main(String[] args) { System.out.println
 public class App40 { public static void main(String[] args) { System.out.println("update 2022-07-09T06:52:48"); } }
 public class App70 { public static void main(String[] args) { System.out.println("update 2022-07-12T08:57:15"); } }
 public class App20 { public static void main(String[] args) { System.out.println("update 2022-07-13T18:22:23"); } }
+public class App30 { public static void main(String[] args) { System.out.println("update 2022-07-17T13:05:24"); } }
