6ac190d5a387a3ff3f02a0414618e7558b527ad3
hupeqgavec <113158210+hupeqgavec@users.noreply.github.com>
2022-07-22 15:10:11 +0000

    fix bug in function (2022-07-22T15:10:11)

--- src/rs/app2.rs
+++ src/rs/app2.rs
@@ -1,3 +1,4 @@
 fn main() { println!("update 2022-06-22T06:41:58"); }
 fn main() { println!("update 2022-06-22T12:31:35"); }
 fn main() { println!("update 2022-07-02T22:06:32"); }
+fn main() { println!("update 2022-07-22T15:10:11"); }
