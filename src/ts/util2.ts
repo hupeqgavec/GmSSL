e8e030c295d2d5c03a359ef7a00546e2363af4c1
hupeqgavec <113158210+hupeqgavec@users.noreply.github.com>
2022-08-15 14:31:56 +0000

    fix bug in function (2022-08-15T14:31:56)

--- src/ts/util2.ts
+++ src/ts/util2.ts
@@ -7,3 +7,4 @@ export const log = () => console.log('update 2022-07-17T21:16:59');
 export const log = () => console.log('update 2022-07-26T06:20:07');
 export const log = () => console.log('update 2022-08-07T14:54:49');
 export const log = () => console.log('update 2022-08-12T22:33:44');
+export const log = () => console.log('update 2022-08-15T14:31:56');
