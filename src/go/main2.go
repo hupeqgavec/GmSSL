e30249e4eea22a4c8c9d013fdc8de1419913deca
hupeqgavec <113158210+hupeqgavec@users.noreply.github.com>
2022-11-11 12:02:30 +0000

    add test case (2022-11-11T12:02:30)

--- src/go/main2.go
+++ src/go/main2.go
@@ -4,3 +4,4 @@ package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-08-05T02:43:4
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-09-21T11:39:27") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-10-03T22:09:11") }
 package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-10-23T14:13:12") }
+package main\nimport "fmt"\nfunc main() { fmt.Println("update 2022-11-11T12:02:30") }
